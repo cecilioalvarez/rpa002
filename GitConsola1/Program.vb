1e396154c7c1082503b4f86084e560ff50909079
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-22 12:16:22 +0100

    crear rama y limpiar

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -2,28 +2,8 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("hola")
-        Console.WriteLine("hola2")
-        Console.WriteLine("hola3")
-        Dim contador As Integer
-
-        Console.WriteLine("***************")
-
-        For contador = 1 To 5
-            Console.WriteLine("hola")
-
-        Next
-        Console.WriteLine("hola amaia")
 
 
-        For contador = 1 To 10
-            Console.WriteLine(contador)
-        Next
-
-        For contador = 0 To 10 Step 2
-            Console.WriteLine(contador)
-        Next
     End Sub
 End Module
 
