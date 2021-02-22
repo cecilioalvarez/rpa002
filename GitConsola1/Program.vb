05360c65446b9f1a7436d90e618d51d5d46c6b38
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-22 10:05:46 +0100

    ok bucle 1

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -6,6 +6,14 @@ Module Program
         Console.WriteLine("hola")
         Console.WriteLine("hola2")
         Console.WriteLine("hola3")
+        Dim contador As Integer
+
+        Console.WriteLine("***************")
+
+        For contador = 0 To 5
+            Console.WriteLine("hola")
+
+        Next
 
 
     End Sub
