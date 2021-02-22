63efc7fdc7417c15c893fac9e88f9db663ec2030
SergioGuerra55 <79444024+SergioGuerra55@users.noreply.github.com>
2021-02-22 14:21:25 +0100

    Update Program.vb

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -21,7 +21,39 @@ Module Program
         Next
 
         Console.WriteLine("rama de sergio")
+Imports System
 
+Module Program
+    Sub Main(args As String())
+        For contador = 1 To 5
+            Console.WriteLine("hola")
+        Next
+
+        For contador = 1 To 10
+            Console.WriteLine(contador)
+        Next
+        For contador = 0 To 10 Step 2
+            Console.WriteLine(contador)
+        Next
+        'tabla de multiclicar del 7
+        For numero1 = 1 To 10
+
+        Next
+        Console.WriteLine(numero1 * 7)
+
+        'sumar 10 primeros numeros
+        For suma = 0 To 9
+            Console.WriteLine(suma + 1)
+        Next
+
+        'sumar numeros pares
+        Dim sumapar As Integer = 2
+        For numeropar As Integer = 0 To 10
+            sumapar = sumapar + numeropar
+        Next
+        Console.WriteLine(sumapar)
+    End Sub
+End Module
 
     End Sub
 End Module
