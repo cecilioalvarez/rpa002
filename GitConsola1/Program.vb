396389c575272eeada2d97ae368cb564f9d04b02
Yessica39Alvarez <79421789+Yessica39Alvarez@users.noreply.github.com>
2021-02-22 12:19:12 +0100

    Update Program.vb
    cambio

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -1,9 +1,36 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
-
-
-    End Sub
+    Sub Main(args As String())mports System
+				
+Public Module Module1
+	Public Sub Main()
+		Console.WriteLine("Hello World")
+		console.WriteLine("hola soy jessica")
+		console.WriteLine("contador")
+		dim contador as integer 
+		for contador = 1 to 10
+		console.WriteLine("contador")
+		console.WriteLine()
+	next
+            Public Module Module1
+	Public Sub Main()
+		Console.WriteLine("Hello World")
+		console.WriteLine("hola soy jessica")
+		console.WriteLine("contador")
+		dim contador as integer 
+		for contador = 1 to 10
+		console.WriteLine("contador")
+		console.WriteLine()
+	next
+		
+		
+	End Sub
 End Module
+		
+		
+	End Sub
+End Module
+
+
 
