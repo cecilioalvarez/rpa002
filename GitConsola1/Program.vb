4325e3f2352321786f54f2825964febbfc575ce8
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-22 12:34:38 +0100

    bucle comensales

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -2,27 +2,41 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("hola")
-        Console.WriteLine("hola2")
-        Console.WriteLine("hola3")
-        Dim contador As Integer
+        'Console.WriteLine("Hello World!")
+        'Console.WriteLine("hola")
+        'Console.WriteLine("hola2")
+        'Console.WriteLine("hola3")
+        'Dim contador As Integer
 
-        Console.WriteLine("***************")
+        'Console.WriteLine("***************")
 
-        For contador = 1 To 5
-            Console.WriteLine("hola")
+        'For contador = 1 To 5
+        '    Console.WriteLine("hola")
 
-        Next
+        'Next
 
         ' mas sencillo de vb.net
         ' bucle va de uno en uno 
-        For contador = 1 To 10
-            Console.WriteLine(contador)
-        Next
-        ' este otro bucle va de dos en dos
-        For contador = 0 To 10 Step 2
-            Console.WriteLine(contador)
+        'For contador = 1 To 10
+        '    Console.WriteLine(contador)
+        'Next
+        '' este otro bucle va de dos en dos
+        'For contador = 0 To 10 Step 2
+        '    Console.WriteLine(contador)
+
+        'Next
+
+        Console.WriteLine("hola")
+        ' el contador empieza en 1
+        ' hay que llegar al 10
+        ' el incremento de cada vuelta es 
+        ' de 7 en 7 
+
+
+        For comensal = 1 To 4 Step 2
+            Console.WriteLine(comensal)
+            Console.WriteLine("pareja admitida")
+            Console.WriteLine("********************")
 
         Next
 
