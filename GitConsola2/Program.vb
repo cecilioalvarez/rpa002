1056da07c6f7a8a3ba374d0a1c74f3ab575a172c
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-22 13:50:05 +0100

    Iteraciones multiples

--- GitConsola2/Program.vb
+++ GitConsola2/Program.vb
@@ -9,11 +9,21 @@ Module Program
 
         'Next
 
-        Dim numero As Integer = 0
-        numero = numero + 1
-        numero = numero + 2
-        numero = numero + 3
-        Console.WriteLine(numero)
+        'Dim numero As Integer = 0
+        'numero = numero + 1
+        'numero = numero + 2
+        'numero = numero + 3
+        'Console.WriteLine(numero)
+        Dim bolsa As Integer = 0
+
+
+        For numero As Integer = 0 To 10
+            bolsa = bolsa + numero
+            Console.WriteLine("la bolsa con iteracion: " & numero & " contiene " & bolsa)
+        Next
+        Console.WriteLine(bolsa)
+
+
 
 
     End Sub
