81f2f8ab39f4e5bc994d55cf921cc90b2755dd92
Yessica39Alvarez <79421789+Yessica39Alvarez@users.noreply.github.com>
2021-02-22 21:07:23 +0100

    Update Program.vb
    cambios de jercicio de pagina ! y tabla de multiplicar

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -29,6 +29,22 @@ Module Program
 	End Sub
 End Module
 
+Imports System
+				
+Public Module Module1
+	Public Sub Main()
+		Console.WriteLine("Hello World")
+		dim numero as integer = 3
+		console.WriteLine(numero)
+		for numero = 0 to 10 
+		console.WriteLine(numero * 3 )
+	next  
+	console.WriteLine("numero")
+		
+		
+	End Sub
+End Module
+
 
 
 
