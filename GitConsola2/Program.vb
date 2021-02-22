b9c1ba5033e9abeec00cfbbfd6bfdbcfb5f14bb0
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-22 13:14:03 +0100

    sumatorio

--- GitConsola2/Program.vb
+++ GitConsola2/Program.vb
@@ -2,6 +2,19 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        'Dim numero As Integer
+
+        'For numero1 As Integer = 1 To 10
+        '    Console.WriteLine(numero1 * 7)
+
+        'Next
+
+        Dim numero As Integer = 0
+        numero = numero + 1
+        numero = numero + 2
+        numero = numero + 3
+        Console.WriteLine(numero)
+
+
     End Sub
 End Module
