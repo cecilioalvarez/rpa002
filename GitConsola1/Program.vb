559d475c80469ca11b3629eb08d2074b395253f2
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-22 09:32:02 +0100

    nueva linea

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -4,6 +4,7 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("hola")
+        Console.WriteLine("hola2")
 
     End Sub
 End Module
