f4be4d25bb9818305f388dc7bcdc88020b246177
Gontzal Bilbao <79211321+gontzalbbilbao@users.noreply.github.com>
2021-02-22 11:56:59 +0100

    Update Program.vb

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -2,25 +2,13 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("hola")
-        Console.WriteLine("hola2")
-        Console.WriteLine("hola3")
-        Dim contador As Integer
+        
+       Dim contador As Integer
 
-        Console.WriteLine("***************")
-
-        For contador = 1 To 5
-            Console.WriteLine("hola")
-
-        Next
-
-
-        For contador = 1 To 10
+        For contador = 1 To 10 Step 2
             Console.WriteLine(contador)
-        Next
 
-        Console.WriteLine("rama de gontzal")
+        Next
 
 
     End Sub
