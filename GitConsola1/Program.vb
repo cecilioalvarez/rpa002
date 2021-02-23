73f8171c2dc6ca9e1d1d9ec07d85b40a4728e782
SergioGuerra55 <79444024+SergioGuerra55@users.noreply.github.com>
2021-02-23 11:45:32 +0100

    Update Program.vb

--- GitConsola1/Program.vb
+++ GitConsola1/Program.vb
@@ -68,7 +68,51 @@ Module Program
             Console.WriteLine(numero)
             numero = numero + 1
         Loop
+        'suma de varios elementos
+        Dim suma As Decimal
+        Dim indice As Integer
 
+        Do While indice < 10
+            indice = indice + 1
+            suma = indice + suma
+        Loop
+        Console.WriteLine(suma)
+
+        'forma de hacer paquetes
+        Dim notas(3) As Decimal
+        notas(0) = 7
+        notas(1) = 4
+        notas(2) = 3
+        Console.WriteLine(notas(0))
+        Console.WriteLine(notas(1))
+        Console.WriteLine(notas(2))
+        'otra forma de hacer paquetes: Dim strings() As String = {"abc", "def", "ghi"}
+        'en el strings() no hace falta poner numero
+        'aplicar a los pack un bucle for
+        Dim paquete(3) As Integer
+        paquete(0) = 8
+        paquete(1) = 2
+        paquete(2) = 1
+        For numero As Integer = 0 To 2
+            Console.WriteLine(paquete(numero))
+        Next
+        'imprimir notas aprovadas
+        Dim examen(10) As Integer
+        examen(0) = 5
+        examen(1) = 3
+        examen(2) = 2
+        examen(3) = 8
+        examen(4) = 4
+        examen(5) = 6
+        examen(6) = 7
+        examen(7) = 9
+        examen(8) = 2
+        examen(9) = 1
+        For resultado As Integer = 0 To 10
+            If examen(resultado) > 5 Then
+                Console.WriteLine("has aprovado " & examen(resultado))
+            End If
+        Next
 
     End Sub
 End Module
