044668007fa0efd4ab04fe576e92c4186d2a0ce0
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-23 13:34:16 +0100

    media

--- GitConsola6/Program.vb
+++ GitConsola6/Program.vb
@@ -3,5 +3,22 @@ Imports System
 Module Program
     Sub Main(args As String())
 
+        Dim misNotas() As Decimal = {4.5, 2.2, 7.2, 8.4}
+        'media sumatorioNEllementos/total
+        'conozco el total de elementos
+        Console.WriteLine(misNotas.Length)
+
+        Dim sumaTodos As Integer = 0
+
+        For i As Integer = 0 To misNotas.Length - 1
+
+            'Console.WriteLine(misNotas(i))
+            sumaTodos = sumaTodos + misNotas(i)
+
+
+        Next
+
+        Console.WriteLine(sumaTodos / misNotas.Length)
+
     End Sub
 End Module
