5e85f8ba2fa370718a42a4e868ff379fac3de44d
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-23 09:47:29 +0100

    bucle while

--- GitConsola3/Program.vb
+++ GitConsola3/Program.vb
@@ -3,12 +3,21 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("introudce un numero")
-        Dim numero As Integer = Console.ReadLine()
-        For i As Integer = 0 To 10
+        'leer el dato por la consola
+        'Dim numero As Integer = Console.ReadLine()
+        'For i As Integer = 0 To 10
+        '    ' le uso en este bloque para generar la  multiplicacion
+        '    Console.WriteLine(numero & "x" & i & "=" & (i * numero))
 
-            Console.WriteLine(numero & "x" & i & "=" & (i * numero))
+        'Next
+        Dim indice As Integer = 0
+        ' un bucle while 
 
-        Next
+        Do While indice < 10
+            Console.WriteLine(indice)
+            indice = indice + 1
+        Loop
+
 
 
     End Sub
