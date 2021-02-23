8a95e0c325ae55626b40a921594bec82ae5bdc35
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-23 11:42:57 +0100

    bucle con if

--- GitConsola4/Program.vb
+++ GitConsola4/Program.vb
@@ -11,10 +11,21 @@ Module Program
         notas(2) = 8
 
         For numero As Integer = 0 To 2
+
             Console.WriteLine(notas(numero))
 
         Next
 
+        Console.WriteLine("***********************")
+
+        ' recorro todos
+        For numero As Integer = 0 To 2
+            ' segundo al recorrer comparo las notas y valoro
+            If notas(numero) >= 5 Then
+                Console.WriteLine(notas(numero))
+            End If
+
+        Next
 
 
 
