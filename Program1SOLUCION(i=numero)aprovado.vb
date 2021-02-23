4a6473ddc2eaf096d0e26b3301bc4949e6c4fef4
AmaiaAizpurua <79261932+AmaiaAizpurua@users.noreply.github.com>
2021-02-23 11:43:40 +0100

    Add files via upload
    aprovado =>5

---
+++ Program1SOLUCION(i=numero)aprovado.vb
@@ -0,0 +1,27 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        'array o lista de notas y el tamaño del array
+        Dim notas(3) As Decimal
+
+        notas(0) = 7
+        notas(1) = 4
+        notas(2) = 8
+
+        For numero As Integer = 0 To 2
+            Console.WriteLine(notas(numero))
+        Next
+
+
+        Console.WriteLine("*************")
+        For numero As Integer = 0 To 2
+            'al recorrer comparo las notas y valoro
+            If notas(numero) >= 5 Then
+                Console.WriteLine(notas(numero))
+            End If
+
+        Next
+    End Sub
+End Module
