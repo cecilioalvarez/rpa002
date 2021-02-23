e6ad326a55f7fb824e4836f4dc920ab676253e84
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-23 11:55:23 +0100

    moficaciones de tatiana

--- GitConsola5/Program.vb
+++ GitConsola5/Program.vb
@@ -10,18 +10,38 @@ Module Program
         notas(1) = 4
         notas(2) = 8
 
-        For numero As Integer = 0 To 2
-            Console.WriteLine(notas(numero))
-        Next
+        'For numero As Integer = 0 To 2
+        '    Console.WriteLine(notas(numero))
+        'Next
 
 
-        Console.WriteLine("*************")
+        'Console.WriteLine("*************")
+        'For numero As Integer = 0 To 2
+        '    'al recorrer comparo las notas y valoro
+        '    If notas(numero) >= 5 Then
+        '        Console.WriteLine(notas(numero))
+        '    End If
+
+        'Next
+
+
+        Dim nota(3) As Integer
+        nota(0) = 3
+        nota(1) = 6
+        nota(2) = 9
+        Console.WriteLine("los aprobados")
         For numero As Integer = 0 To 2
-            'al recorrer comparo las notas y valoro
-            If notas(numero) >= 5 Then
-                Console.WriteLine(notas(numero))
+
+            If nota(numero) >= 5 Then
+                Console.WriteLine(nota(numero))
             End If
 
+
         Next
+
+
+
+
+
     End Sub
 End Module
