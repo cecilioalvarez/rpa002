aeb6639c7cdae9add04be98bea1011cf27fc0d22
cecilio Alvarez <cmac@arquitecturajava.com>
2021-02-23 10:56:34 +0100

    arrays

---
+++ GitConsola4/Program.vb
@@ -0,0 +1,21 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        ' array o lista de notas y el tamaño del array
+        Dim notas(3) As Decimal
+
+        notas(0) = 7
+        notas(1) = 4
+        notas(2) = 8
+
+        Console.WriteLine(notas(0))
+        Console.WriteLine(notas(1))
+        Console.WriteLine(notas(2))
+
+
+
+
+    End Sub
+End Module
